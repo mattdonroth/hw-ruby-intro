56fdad6ae5fa775d4861b342ee152457ab792e9b
MRoth <mroth@sandiego.edu>
2018-09-11 23:09:45 -0700

    part 3 left

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,50 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.empty?
+      return 0
+  end
+  return arr.max(2).reduce(:+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return arr.permutation(2).any? { |a,b| a + b == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, "+name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return s =~ /\A(?=[^aeiou])(?=[a-z])/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.size == 0
+    return false
+  end
+  if s.gsub(/[01]/, '') == ''
+    num = s.to_i
+    return num%4 == 0
+  end
+  return false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    return "$"+price
+  end
 end
